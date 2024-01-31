82b00bbf4c4f795b2c99d1ae0007406788c0847d
Thomas Calmant <thomas.calmant@kentyou.com>
2024-01-31 17:25:06 +0100

    Updated build script

--- sensinact-enabler/docker-image/build.sh
+++ sensinact-enabler/docker-image/build.sh
@@ -3,6 +3,12 @@
 IMAGE=${IMAGE:-"sensinact"}
 TAG=${TAG:-"0.0.2-SNAPSHOT-CHEAAS"}
 
-mvn -f data/pom.xml clean prepare-package || exit 1
+echo "Building Health endpoint..."
+mvn -f ../../rest-health-endpoint/pom.xml clean install || exit 1
 
+echo "Constructing repository..."
+mvn -f pom.xml clean prepare-package || exit 1
+
+echo "Building image..."
 docker build -t "$IMAGE:$TAG" . || exit 1
+echo "Done."
